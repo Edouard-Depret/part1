760be857adb3f8f0119cd7119fcf7c453c200032
Edouard Depret <edouarddepret@gmail.com>
2022-12-19 17:22:13 +0100

    Exercice 1.1 and 1.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-function App() {
+  const Header = (course) => {
+    return (
+      <h1>
+        {course.title}
+      </h1>
+    )
+  }
+
+  const Part = (part) => {
+    return (
+      <div>
+        <p>{part.name} {part.number}</p>
+      </div>
+    )
+  }
+
+  const Content = () => {
+    return (
+      <div>
+        <Part name={part1} number={exercises1}/>
+        <Part name={part2} number={exercises2}/>
+        <Part name={part3} number={exercises3}/>
+      </div>
+    )
+  }
+
+  const Total =() => {
+    return (
+      <div>
+        <p> Number of exercices: {exercises1 + exercises2 + exercises3}</p>
+      </div>
+    )
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title={course}/>
+      <Content />
+      <Total />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
